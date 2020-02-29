6bfdfaa97d84d8317b2897fcbefdfc0faa98324a
Jisan Jung <jjjung13@gmail.com>
2020-02-28 22:50:41 -0500

    working on testimonial section

--- src/script.js
+++ src/script.js
@@ -1,9 +1,38 @@
 (function() {
 //---------------------------------------------------------------
 
-$("#calendar").flatpickr({
-    inline: true
-});
+var calendar = {
+    init: function() { // main method
+        this.cacheDOM();
+        this.render();
+    },
+    cacheDOM: function() { // grab DOM elements
+        this.$calendar = $("#calendar");
+    },
+    render: function() { // only touches the html
+        this.$calendar.flatpickr({
+            inline: true
+        });
+    }
+};
+calendar.init();
+
+
+var testimonials = {
+    init: function() { // main method
+        this.cacheDOM();
+        this.render();
+    },
+    cacheDOM: function() { // grab DOM elements
+        this.$slideshow = $(".slideshow-container");
+    },
+    render: function() { // only touches the html
+        this.$slideshow.slick({
+            dots: true
+        });
+    }
+};
+testimonials.init();
 
 //---------------------------------------------------------------
 })();
